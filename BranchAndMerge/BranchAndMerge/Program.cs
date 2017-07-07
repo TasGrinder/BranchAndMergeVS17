6cd0ed87dc921d5a9bdd250a11e7d1af216d4b64
Jake Porter <jakeporter05@gmail.com>
2017-07-07 15:48:52 -0600

    first check in

---
+++ BranchAndMerge/BranchAndMerge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchAndMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We be branching");
+        }
+    }
+}
